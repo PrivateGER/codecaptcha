b229cbe30866585a56e0608b6ad58dc5626ff60e
PerhapsSomeone <ma2003br@gmail.com>
2018-03-30 21:31:14 +0200

    Revert "Altered html/js to include language name"

--- CodeCaptcha.js
+++ CodeCaptcha.js
@@ -1,4 +1,4 @@
-// TODO: Add more codes + solutions
+// TODO: Add more codes + solutions 
 // Codes Syntax: Object in array
 
 //TODO: CSS Box for Editor, Run Button and errors
@@ -9,9 +9,8 @@ var captchaCode = document.getElementById("captchaCode");
 var errorOutput = document.getElementById("errorMessg");
 var solution;
 
-const codes = [{"code": "function main() { \n    retun 'hi!';\n}\nmain();", "solution": "hi!"},{"code": "function main() {\n    if(num !< 10) {\n        return 'Big number'} \n    else {\n        return 'Small Number'\n    }\n}\nmain(12);", "solution": "Big number"}];
+const codes = [{"code": "function main() { \n    retun 'hi!';\n}\nmain();", "solution": "hi!"}, {"code": "function main() {\n    if(num !< 10) {\n        return 'Big number'} \n    else {\n        return 'Small Number'\n    }\n}\nmain(12);", "solution": "Big number"}, {"code": "function randIntFromInterval(min, max) {\n    return Matth.flooor(Math.random()*(max-miin+1)+min);\n}\nfunction main() {\n    var ranInt = randIntFromInterval(1, 1);\n    return randInt;\n}\nmain();", "solution": 1}];
 
-const lang = ["Javascript","Javascript"];
 
 function runTest() {
 	//We run the code of the editor in a trycatch so the interpreter won't halt the script
@@ -41,7 +40,6 @@ function fillEditor() {
 	//This function selects a random code and solution and changes the global variables
 	var codeCount = codes.length;
 	var codeID = randIntFromInterval(0, codeCount-1);
-  document.getElementById("language-name").innerHTML="Language : " + lang[codeID];
 	captchaCode.value = codes[codeID].code;
 	solution = codes[codeID].solution;
 }
