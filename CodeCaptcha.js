de52c40207579a7a3fec3c328cbac3503dd49719
PerhapsSomeone <ma2003br@gmail.com>
2018-03-30 16:55:57 +0200

    Added some basic comments

--- CodeCaptcha.js
+++ CodeCaptcha.js
@@ -15,7 +15,7 @@ const codes = [{"code": "function main() {retun 'hi!'; } main();", "solution": "
 function runTest() {
 	//We run the code of the editor in a trycatch so the interpreter won't halt the script
 	try {
-		var output = eval(codeArea.value); 
+		var output = eval(codeArea.value); //Gets the printed output of the script
 	} 
 	catch(err) {
 		errorOutput.innerHTML = err;
@@ -23,13 +23,14 @@ function runTest() {
 
 	if(output === solution) {
 		alert("Successfully completed!");
-		document.getElementById("submit").disabled = false;
+		document.getElementById("submit").disabled = false; //TODO: Change hardcoded ID
 	} else {
-		alert("Wrong answer. Your code returns " + output);
+		alert("Wrong answer. Your code returns " + output); 
 	}
 }
 
 function fillEditor() {
+	//This function selects a random code and solution and changes the global variables
 	var codeCount = codes.length;
 	var codeID = randIntFromInterval(0, codeCount);
 	codeArea.value = codes[codeID].code;
