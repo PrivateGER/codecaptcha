1c521f02eb123287755c217e0c2410cf30bfbcdd
PerhapsSomeone <ma2003br@gmail.com>
2018-03-31 11:00:19 +0200

    Added new puzzle

--- CodeCaptcha.js
+++ CodeCaptcha.js
@@ -13,8 +13,10 @@ var solution;
 
 const codes = [{"code": "function main() { \n    retun 'hi!';\n}\nmain();", "solution": "hi!"},
 {"code": "function main() {\n    if(num !< 10) {\n        return 'Big number'} \n    else {\n        return 'Small Number'\n    }\n}\nmain(12);", "solution": "Big number"},
-{"code": "function randIntFromInterval(min, max) {\n    return Matth.flooor(Math.random()*(max-miin+1)+min);\n}\nfunction main() {\n    var ranInt = randIntFromInterval(1, 1);\n    return randInt;\n}\nmain();", "solution": 1}];
-const lang = ["Javascript","Javascript","Javascript"];
+{"code": "function randIntFromInterval(min, max) {\n    return Matth.flooor(Math.random()*(max-miin+1)+min);\n}\nfunction main() {\n    var ranInt = randIntFromInterval(1, 1);\n    return randInt;\n}\nmain();", "solution": 1},
+{"code": "function getUsername(user) {\n    return user[name];\n}\n \nfunction getTel() {\n    return user[tel];\n}\nuser = {'name': 'Daniel', 'Tel': '+23 54536536536'};\ngetTel(user);\n", "solution": "+23 54536536536"}];
+
+const lang = ["Javascript","Javascript","Javascript", "Javascript"];
 
 function runTest() {
 	//We run the code of the editor in a trycatch so the interpreter won't halt the script
